05262d2de0e93490135e9ece73b77856fdbd0b09
Ray Walters <rwalters@ezpublishing.com>
2012-05-13 08:06:13 -0700

    Set up the layout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Libre::Application.config.secret_token = '39a11811a4479005c8c4e2a8397db45dda6846d3a60c03abb9ffb4e17f572c0f109c6d06c6e53e4c1d81bb37d4b09dfc986f524586b80dc50b59769570b6df46'
